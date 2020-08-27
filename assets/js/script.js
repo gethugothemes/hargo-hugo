7f7a4a1c899ae0ef45547af90a90ee64a8fc174c
somratpro <abdulmonnafsomrat@gmail.com>
2020-08-27 13:07:11 +0600

    modified preloader

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
+// Preloader js    
+$(window).on('load', function () {
+  $('.preloader').fadeOut(100);
+});
+
 (function ($) {
   'use strict';
-
-  // Preloader js    
-  $(window).on('load', function () {
-    $('.preloader').fadeOut(100);
-  });
 
   // navigation
   $(window).scroll(function () {
@@ -79,7 +79,7 @@
       return '<img class="img-fluid" src="' + image + '" alt="product-img">';
     }
   });
-  
+
 
   // Accordions
   $('.collapse').on('shown.bs.collapse', function () {
